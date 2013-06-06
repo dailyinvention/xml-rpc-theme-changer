db58a089f9626142a13cd477762eb5e5846382bf
dailyinvention <stefan@dailyinvention.com>
2013-06-05 22:50:50 -0400

    Allow WordPress Editors to make calls.

--- xml-rpc-theme-changer.php
+++ xml-rpc-theme-changer.php
@@ -7,6 +7,28 @@ Author: Stefan Holodnick
 Author URI: http://blog.dailyinvention.com
 Version: 1.0
 */
+//Enables editor access to necessary functions when plugin is enabled
+
+function enable_theme_roles () {
+
+        $editor = get_role('editor');
+        $editor->add_cap('wp_get_themes');
+        $editor->add_cap('get_current_theme');
+        $editor->add_cap('switch_theme');
+
+}
+
+//Disables editor access to necessary functions when plugin is disabled
+
+function disable_theme_roles () {
+
+        $editor = get_role('editor');
+        $editor->remove_cap('wp_get_themes');
+        $editor->remove_cap('get_current_theme');
+        $editor->remove_cap('switch_theme');
+
+}
+
 
 // Returns an array of themes in Wordpress
 function theme_get_themes($params) {
@@ -73,6 +95,8 @@ function theme_methods($methods) {
 }
 
 // Adds methods to Wordpress's XML-RPC methods
+register_activation_hook( __FILE__, 'enable_theme_roles' );
+register_deactivation_hook( __FILE__, 'disable_theme_roles' );
 add_filter('xmlrpc_methods', 'theme_methods');
 
 ?>
